38acf7c554a456446d0766f5d455a1411e3ef5bc
IkrarAB <ikraranugrah26@gmail.com>
2019-09-23 23:11:38 +0700

    Initial commit

---
+++ src/pbo1/pkg10118033/latihan12/PBO110118033Latihan12.java
@@ -0,0 +1,29 @@
+package pbo1.pkg10118033.latihan12;
+
+/*
+ *
+ * @author
+ * NAMA  : Ikrar Anugrah Bastary
+ * KELAS : PBO1
+ * NIM   : 10118033
+ *
+ */
+
+public class PBO110118033Latihan12 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a - b = " + (a-b));
+        System.out.println("a * b = " + (a*b));
+        System.out.println("b / a = " + (b/a));
+        System.out.println("b % a = " + (b%a));
+        System.out.println("c % a = " + (c%a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (b--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+    }
+}
